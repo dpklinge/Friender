bf02b166b325150a1130475e2473d47b4832cea5
Daniel Klingensmith <dpklinge@ncsu.edu>
2023-01-29 19:09:24 -0500

    Added missing validation files

---
+++ src/main/kotlin/com/clearintentions/friender/validators/SpringEmailValidator.kt
@@ -0,0 +1,34 @@
+package com.clearintentions.friender.validators
+
+import org.apache.commons.validator.routines.EmailValidator
+import javax.validation.Constraint
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+import javax.validation.Payload
+import kotlin.reflect.KClass
+
+
+class SpringEmailValidator : ConstraintValidator<ValidEmail, String> {
+    override fun isValid(email: String, context: ConstraintValidatorContext
+    ) : Boolean{
+        return if(!EmailValidator.getInstance().isValid(email)) {
+            //This disables the default message provided in @ValidEmail and supplies a new message.
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Invalid email provided.").addConstraintViolation();
+            false;
+        }else{
+            true
+        }
+    }
+
+}
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [SpringEmailValidator::class])
+@MustBeDocumented
+annotation class ValidEmail(
+    val message: String = "Invalid email provided.",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
